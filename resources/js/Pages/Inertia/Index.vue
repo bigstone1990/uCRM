7383c05b66e59df03723880a45697a0cfe61647c
bigstone1990 <masatakalman@gmail.com>
2023-08-07 13:30:12 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    blogs: Array
+})
+</script>
+
+<template>
+    <div v-if="$page.props.flash.message" class="bg-blue-300">
+    {{ $page.props.flash.message }}
+    </div>
+    <ul>
+        <li v-for="blog in blogs" v-bind:key="blog.id">
+            件名: <Link class="text-blue-400" v-bind:href="route('inertia.show', { id: blog.id })">{{ blog.title }}</Link>,
+            本文: {{ blog.content }}
+        </li>
+    </ul>
+    これはテストです。
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const newTitle = ref('');
+const newContent = ref('');
+</script>
+
+<template>
+    Inertiaテストです。<br>
+    <a href="/">aタグ経由です。</a><br>
+    <Link href="/">Link経由です。</Link><br>
+    <Link v-bind:href="route('inertia.index')">名前付きルートの確認</Link><br>
+    <Link v-bind:href="route('inertia.show', { id: 1 })">ルートパラメータのテストです</Link>
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle"><br>
+    <input type="text" name="newContent" v-model="newContent"><br>
+    <Link as="button" method="post" v-bind:href="route('inertia.store')" v-bind:data="{ title: newTitle, content: newContent }">DB保存テスト</Link>
+</template>
